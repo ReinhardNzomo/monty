02e81243b708d4cd30f05c4a038162dacd30f823
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-15 12:50:15 +0300

    all again

--- monty.h
+++ monty.h
@@ -7,9 +7,9 @@
 
 #define STACK 0
 #define QUEUE 1
-#define DELIMS " /n/t/a/b"
+#define DELIMS " \n\t\a\b"
 
-/* GLOBAL OPCODE TOKENS*/
+/* GLOBAL OPCODE TOKENS */
 extern char **op_toks;
 
 /**
@@ -34,7 +34,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO ALX Holberton project
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -48,7 +48,7 @@ int init_stack(stack_t **stack);
 int check_mode(stack_t *stack);
 void free_tokens(void);
 unsigned int token_arr_len(void);
-int run_montly(FILE *script_fd);
+int run_monty(FILE *script_fd);
 void set_op_tok_error(int error_code);
 
 /* OPCODE FUNCTIONS */
@@ -62,13 +62,13 @@ void monty_nop(stack_t **stack, unsigned int line_number);
 void monty_sub(stack_t **stack, unsigned int line_number);
 void monty_div(stack_t **stack, unsigned int line_number);
 void monty_mul(stack_t **stack, unsigned int line_number);
+void monty_mod(stack_t **stack, unsigned int line_number);
 void monty_pchar(stack_t **stack, unsigned int line_number);
 void monty_pstr(stack_t **stack, unsigned int line_number);
 void monty_rotl(stack_t **stack, unsigned int line_number);
 void monty_rotr(stack_t **stack, unsigned int line_number);
 void monty_stack(stack_t **stack, unsigned int line_number);
 void monty_queue(stack_t **stack, unsigned int line_number);
-void monty_mod(stack_t **stack, unsigned int line_number);
 
 /* CUSTOM STANDARD LIBRARY FUNCTIONS */
 char **strtow(char *str, char *delims);
@@ -78,12 +78,12 @@ char *get_int(int n);
 int usage_error(void);
 int malloc_error(void);
 int f_open_error(char *filename);
-int unkown_op_error(char *opcode, unsigned int line_number);
+int unknown_op_error(char *opcode, unsigned int line_number);
 int no_int_error(unsigned int line_number);
 int pop_error(unsigned int line_number);
 int pint_error(unsigned int line_number);
 int short_stack_error(unsigned int line_number, char *op);
 int div_error(unsigned int line_number);
-int pchar_error(unsigned int line_number char *message);
+int pchar_error(unsigned int line_number, char *message);
 
 #endif /* __MONTY_H__ */
